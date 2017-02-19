86dc14bbd57460fb877669144829452a7bb31eb3
Ulrich Jansen <ulrich.jansen@rwth-aachen.de>
2017-02-19 17:08:08 +0100

    restart django 1.10.5

--- PiBlaster3/wsgi.py
+++ PiBlaster3/wsgi.py
@@ -1,16 +1,16 @@
 """
-WSGI config for piblaster3 project.
+WSGI config for PiBlaster3 project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
 
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "piblaster3.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "PiBlaster3.settings")
 
 application = get_wsgi_application()
