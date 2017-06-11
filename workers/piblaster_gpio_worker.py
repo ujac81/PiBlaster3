9447764f95f726539f2bf6a161a0ca44fe2ac45a
ujac81 <ulrich.jansen@rwth-aachen.de>
2017-06-11 22:23:07 +0300

    gpio started

---
+++ workers/piblaster_gpio_worker.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python3
+""" pyblaster2.py -- Worker daemon for piremote app.
+
+Enable party mode for music player daemon, perform uploads, read/write GPIOs.
+
+Communication to piremote app via SQL database
+
+@Author Ulrich Jansen <ulrich.jansen@rwth-aachen.de>
+"""
+
+import os
+import signal
+import time
+
+from PiBlaster3.settings import *
+from workers.gpio import PB_GPIO, LED, Buttons
+
+
+class PiBlasterGpioWorker:
+    """Worker daemon for piremote"""
+
+    def __init__(self):
+        """Create uploader and mpd service threads."""
+        self.keep_run = True  # run daemon as long as True
+        self.is_vassal = 'UWSGI_VASSAL' in os.environ
+        self.led = LED(self)
+        self.buttons = Buttons(self)
+        
+
+    def run(self):
+        """daemonize, start threads and enter daemon loop."""
+        if not DEBUG and not self.is_vassal:
+            # No fork in debug mode and not in uwsgi vassal mode.
+            self.daemonize()
+        else:
+            print('PiBlasterWorker running in debug or as vassal')
+        
+        PB_GPIO.init_gpio()        
+        self.led.init_leds()
+        self.led.show_init_done()
+        self.buttons.start()
+
+        self.daemon_loop()
+
+        self.buttons.join()
+        self.led.join()
+        PB_GPIO.cleanup(self)
+
+        
+    def print_message(self, msg):
+        """
+
+        :param msg:
+        :return:
+        """
+        if DEBUG or self.is_vassal:
+            print('[WORKER] {0}'.format(msg))
+
+    def daemon_loop(self):
+        """Main daemon loop.
+
+        uploader and party mode threaded,
+        ratings scanner activated if idler found database update.
+
+        :return:
+        """
+
+        while self.keep_run:
+
+            time.sleep(100. / 1000.)  # 100ms
+        
+        self.print_message('LEAVING')
+
+    def term_handler(self, *args):
+        """ Signal handler to stop daemon loop"""
+        self.keep_run = False
+
+    def daemonize(self):
+        """Fork process and install signal handler"""
+
+        signal.signal(signal.SIGTERM, self.term_handler)
+        signal.signal(signal.SIGINT, self.term_handler)
+        try:
+            pid = os.fork()
+        except OSError:
+            print("Failed to fork daemon")
+            raise
+
+        if pid == 0:
+            os.setsid()
+            try:
+                pid = os.fork()
+            except OSError:
+                print("Failed to fork daemon")
+                raise
+
+            if pid == 0:
+                os.chdir("/tmp")
+                os.umask(0)
+            else:
+                exit(0)
+        else:
+            exit(0)
+
+
+if __name__ == '__main__':
+    blaster_worker = PiBlasterGpioWorker()
+    blaster_worker.run()
